05d0fc8b6b22b61a24a6cde3da317adbac474260
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-04-27 10:07:05 +0100

    Jsnack - 1 Html Mod Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// ************ JSnack 1 ***************
 // L’utente inserisce due parole in successione, con due prompt.
 var word_1 = prompt("Inserisci una parola")
 console.log(word_1);
@@ -6,8 +7,13 @@ console.log(word_2);
 // Il software stampa prima la parola più corta, poi la parola più lunga.
 if (word_1.length > word_2.length){
     console.log(word_1);
+    document.getElementById("longest_word").innerHTML = word_1
 } else if (word_1.length < word_2.length) {
     console.log(word_2);
+    document.getElementById("longest_word").innerHTML = word_2
 } else {
     console.log("Try Again");
+    document.getElementById("longest_word").innerHTML = "Try Again!"
 }
+
+// ************ JSnack 2 ***************
