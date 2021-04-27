d57364bcf9f228165fa218cfd0e2c221b4f4888e
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-04-27 10:36:00 +0100

    Last Mod Jsnack 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,11 +6,11 @@ var word_2 = prompt("Inserisci un' altra parola")
 console.log(word_2);
 // Il software stampa prima la parola più corta, poi la parola più lunga.
 if (word_1.length > word_2.length){
-    console.log(word_1);
-    document.getElementById("longest_word").innerHTML = word_1
+    console.log(word_1 , word_2);
+    document.getElementById("longest_word").innerHTML = word_1 + "" + word_2
 } else if (word_1.length < word_2.length) {
-    console.log(word_2);
-    document.getElementById("longest_word").innerHTML = word_2
+    console.log(word_2 , word_1);
+    document.getElementById("longest_word").innerHTML = word_2 + "" + word_1
 } else {
     console.log("Try Again");
     document.getElementById("longest_word").innerHTML = "Try Again!"
