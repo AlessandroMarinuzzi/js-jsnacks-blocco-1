b449ab19d50541e540e2be76863708675c83f99f
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-04-28 10:49:50 +0100

    Snack 3 and 4 mods

--- Snack4/assets/js/main.js
+++ Snack4/assets/js/main.js
@@ -4,11 +4,13 @@ var array = []
 var number;
 var result = document.getElementById("result")
 // Chiedi per 6 volte all’utente di inserire un numero.
-for (var i = 0; [i] < 6; i++) {
-    var number = Number(prompt('insert number'));
-    if ( number % 2 != 0){
+for (var i = 0; i < 6; i++){
+    number = Number(prompt("Inserisci un numero, registrerò i numeri dispari per te."))
+    if ((number % 2) !=0){
         array.push(number);
-     }
- }
- result.innerHTML = number;
+    }
+}
+console.log(array);
+console.log(number);
+result.innerHTML = array;
 // Se è dispari inseriscilo nell’array
