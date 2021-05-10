575997bad64afba86f887105f2b91d1f3424a2fb
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-05-10 10:46:34 +0100

    - Change language to english. - Prompt to change ball weight.

--- Block 2/Snack-1/assets/js/main.js
+++ Block 2/Snack-1/assets/js/main.js
@@ -1,5 +1,9 @@
-// Creare un oggetto palla che abbia le seguenti proprietà.
-var palla = {
-    "Name" : "palla",
+//1.a Creare un oggetto palla che abbia le seguenti proprietà. 
+var ball = {
+    "Name" : "ball",
     Weight : 10
 }
+
+// 1.b Attraverso un prompt dare la possibilità all’utente di modificare il peso della palla.
+
+ball.weight = parseInt(prompt("Change ball weight"));
